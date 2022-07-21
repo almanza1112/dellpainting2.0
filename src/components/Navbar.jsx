94da2c238ce771cbfb70584736a3909156f06266
almanza1112 <almanza1112@gmail.com>
2022-07-21 16:08:16 -0400

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/logo.png'
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+import { FaTimes } from 'react-icons/fa'
+import { HiMenuAlt4 } from 'react-icons/hi'
 
 const Navbar = () => {
+
+    // Setting mobile view
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
     return (
-        <div className='w-full flex justify-between items-center px-4 '>
+        <nav className='w-full h-20 flex justify-between items-center px-4'>
             <img src={Logo} style={{ width: '100px' }} />
 
             {/* Menu */}
-            <ul className='flex'>
+            <ul className='hidden md:flex'>
                 <CustomLink to='/'>HOME</CustomLink>
-
                 <CustomLink to='/services'>SERVICES</CustomLink>
                 <CustomLink to='/portfolio'>PORTFOLIO</CustomLink>
                 <CustomLink to='/careers'>CAREERS</CustomLink>
@@ -18,7 +24,21 @@ const Navbar = () => {
                 <CustomLink to='/contact'>CONTACT</CustomLink>
             </ul>
 
-        </div>
+            {/* Hamburger */}
+            <div onClick={handleClick} className='md:hidden z-10'>
+                {!nav ? <HiMenuAlt4 /> : <FaTimes />}
+            </div>
+
+            {/* Mobile Menu */}
+            <ul className={nav ? 'absolute top-[80px] right-0 pt-4 w-full h-screen bg-white duration-500 border-l-8 border-black' : 'absolute right-[-100%] pt-4 h-screen w-full '}>
+                <CustomLink onClick={nav === true} to='/'>HOME</CustomLink>
+                <CustomLink onClick={nav === true} to='/services'>SERVICES</CustomLink>
+                <CustomLink onClick={nav === true} to='/portfolio'>PORTFOLIO</CustomLink>
+                <CustomLink onClick={nav === true} to='/careers'>CAREERS</CustomLink>
+                <CustomLink onClick={nav === true} to='/associates'>ASSOCIATES</CustomLink>
+                <CustomLink onClick={nav === true} to='/contact'>CONTACT</CustomLink>
+            </ul>
+        </nav>
     )
 }
 
@@ -26,11 +46,10 @@ function CustomLink({ to, children, ...props }) {
     const resolvesPath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvesPath.pathname, end: true })
     return (
-        <li className={isActive ? 'active' : ''}>
-            <Link to={to} {...props}>{children}</Link>
+        <li >
+            <Link to={to} {...props} className={isActive ? 'border-b-4 border-red-600 pb-1' : ''}>{children}</Link>
         </li>
     )
 }
-
 
 export default Navbar
