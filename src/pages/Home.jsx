69c1ea174d53e4b1914a7b70048531a64c3f33d7
almanza1112 <almanza1112@gmail.com>
2022-07-22 02:42:08 -0400

    quick design test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import bgVideo from '../assets/home_vid_bg.mp4'
 
 const Home = () => {
@@ -6,18 +6,23 @@ const Home = () => {
   // video.defaultPlaybackRate = 4
   // video.load()
 
+  const videoRef = useRef();
+  const setPlayBack = () => {
+    videoRef.current.playbackRate = 2.5;
+  }
+
   return (
     <div name='home' className='section-style'>
 
       {/* Container */}
       <div className='w-full justify-center h-full'>
         <div className='overlay'></div>
-        <video src={bgVideo} autoPlay loop muted playsInline className='blur-lg' />
-        <div className='absolute w-full h-full top-0 flex flex-col justify-center p-4 md:p-8'>
-          <p className='text-3xl md:text-6xl font-bold text-white drop-shadow-xl'>PAINTERS YOU CAN TRUST</p>
-          <p className='md:text-3xl my-4 text-white drop-shadow-2xl uppercase'>Over 25 years of experience at your service </p>
+        <video ref={videoRef} onCanPlay={() => setPlayBack()} src={bgVideo} autoPlay loop muted playsInline className='blur-lg' />
+        <div className='absolute w-full h-full top-0 flex flex-col justify-center p-4 md:p-8 items-center md:items-start'>
+          <p className='text-2xl md:text-6xl font-bold text-white drop-shadow-xl'>PAINTERS YOU CAN TRUST</p>
+          <p className='md:text-3xl my-4 text-white drop-shadow-xl'>Over 25 years of experience at your service </p>
           <div className=''>
-            <button className='border-2 border-black p-2 hover:bg-red-600 hover:border-red-600 hover:text-gray-200'>GET IN TOUCH</button>
+            <button className='border-2 border-white font-semibold text-white p-3 hover:bg-red-600 hover:border-red-600 hover:text-gray-200 drop-shadow-xl'>GET IN TOUCH</button>
           </div>
         </div>
       </div>
