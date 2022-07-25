aeb7cfa0c6911f4e016b6704158b5477f733fa0d
almanza1112 <almanza1112@gmail.com>
2022-07-25 15:33:21 -0400

    adjusting

--- src/components/Services/ServicesContent.jsx
+++ src/components/Services/ServicesContent.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import MaintenanceImg from '../../assets/maintenance.jpeg'
 
 const ServicesContent = () => {
     return (
-        <div>
-            hello
+        <div className='w-full flex flex-col p-8 '>
+            <div className='grid md:grid-cols-2'>
+                <div className='flex flex-col justify-center'>
+                    <p className='content-title'> Maintenance</p>
+                    <p>Restoring original color on the walls. Updating Colors and Wallcovering
+                        for a more updated and unique look. Repairing any damages prior to
+                        restoring surfaces.</p>
+                    <p>*Image is Pantone Color of The Year - 2020</p>
+                </div>
+
+                <img src={MaintenanceImg} style={{ width: '400px' }} className='justify-self-center' />
+            </div>
+            <div className='grid md:grid-cols-2'>
+                <div className='flex flex-col justify-center'>
+                    <p className='content-title'> Maintenance</p>
+                    <p>Restoring original color on the walls. Updating Colors and Wallcovering
+                        for a more updated and unique look. Repairing any damages prior to
+                        restoring surfaces.</p>
+                    <p>*Image is Pantone Color of The Year - 2020</p>
+                </div>
+
+                <img src={MaintenanceImg} style={{ width: '400px' }} className='justify-self-center' />
+            </div>
+            <div className='grid md:grid-cols-2'>
+                <div className='flex flex-col justify-center'>
+                    <p className='content-title'> Maintenance</p>
+                    <p>Restoring original color on the walls. Updating Colors and Wallcovering
+                        for a more updated and unique look. Repairing any damages prior to
+                        restoring surfaces.</p>
+                    <p>*Image is Pantone Color of The Year - 2020</p>
+                </div>
+
+                <img src={MaintenanceImg} style={{ width: '400px' }} className='justify-self-center' />
+            </div>
         </div>
     )
 }
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,7 +3,7 @@ import ServicesImg from '../assets/services.jpg'
 
 const Services = () => {
   return (
-    <div className='section-style '>
+    <div className='main-style '>
       <img src={ServicesImg} className='w-full h-full object-cover grayscale' />
       <div className='absolute w-full h-full top-0 flex flex-col justify-center p-4 md:p-8 items-center md:items-start'>
         <p className='text-white text-4xl tracking-widest drop-shadow-lg'>OUR</p>
