7789fe4839c5a49468658b54fbed7e38484ea3cd
almanza1112 <almanza1112@gmail.com>
2022-07-25 19:51:44 -0400

    optimizing

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,15 +3,15 @@ import ServicesImg from '../assets/services.jpg'
 
 const Services = () => {
   return (
-    <div className='w-full md:h-screen'>
-      <img src={ServicesImg} className='w-full h-[60vh] object-cover grayscale' />
-      <div className='max-w-screen-2xl w-full h-[40vh] bottom-0 grid md:grid-cols-3 xl:grid-cols-4 p-4 md:p-8 mx-auto'>
-        <div className='flex flex-col justify-center items-center md:items-start'>
-          <p className='text-red-600 text-xl md:text-4xl md:pb-4'>OUR</p>
-          <p className='text-black text-3xl md:text-5xl'>SERVICES</p>
+    <div className='w-full h-screen'>
+      <img src={ServicesImg} className='main-bg-img' />
+      <div className='main-content'>
+        <div className='main-title-section'>
+          <p className='main-title-top'>OUR</p>
+          <p className='main-title-bottom'>SERVICES</p>
         </div>
 
-        <div className='col-span-1 md:col-span-2 flex flex-col justify-center md:pl-8'>
+        <div className='main-desc-section'>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem id aspernatur, quibusdam accusantium repellendus officiis. Tempora ipsa itaque, dignissimos molestias at doloribus quas, id et, cumque deserunt odio! Consectetur.
           </p>
