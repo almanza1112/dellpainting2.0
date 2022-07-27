133bcdb5f461ce0cfddb39efaf0fa86edc364ca4
almanza1112 <almanza1112@gmail.com>
2022-07-27 16:38:38 -0400

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,12 +12,14 @@ const Home = () => {
 
       {/* Container */}
       <div className='w-full justify-center h-full'>
-        <video ref={videoRef} onCanPlay={() => setPlayBack()} src={bgVideo} autoPlay loop muted playsInline className='blur-lg' />
+        <video ref={videoRef} onCanPlay={() => setPlayBack()} autoPlay loop muted playsInline><source src={bgVideo}/></video>
         <div className='absolute w-full h-full top-0 flex flex-col justify-center p-4 md:p-8 items-center md:items-start'>
-          <p className='text-2xl md:text-6xl font-bold text-white drop-shadow-xl'>PAINTERS YOU CAN TRUST</p>
-          <p className='md:text-3xl my-4 text-white drop-shadow-xl'>Over 25 years of experience at your service </p>
-          <div className=''>
-            <button className='border-2 border-white font-semibold text-white p-3 hover:bg-red-600 hover:border-red-600 hover:text-gray-200 drop-shadow-xl'>GET IN TOUCH</button>
+          <div className='backdrop-blur-lg p-8'>
+            <p className='text-2xl md:text-6xl font-bold text-white drop-shadow-xl'>PAINTERS YOU CAN TRUST</p>
+            <p className='md:text-3xl my-4 text-white drop-shadow-xl'>Over 25 years of experience at your service </p>
+            <div className=''>
+              <button className='border-2 border-white font-semibold text-white p-3 hover:bg-red-600 hover:border-red-600 hover:text-gray-200 drop-shadow-xl'>GET IN TOUCH</button>
+            </div>
           </div>
         </div>
       </div>
