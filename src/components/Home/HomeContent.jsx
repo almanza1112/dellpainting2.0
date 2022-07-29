7f4fd277d34e24f65fdb24ff6ba77238e6262a81
almanza1112 <almanza1112@gmail.com>
2022-07-29 18:52:38 -0400

    home design update

--- src/components/Home/HomeContent.jsx
+++ src/components/Home/HomeContent.jsx
@@ -1,10 +1,79 @@
 import React from 'react'
+import UsaMap from 'react-usa-map'
 
 const HomeContent = () => {
+
+  let mapHandler = (event) => {
+    alert(event.target.dataset.name)
+  }
+
+  let statesCustomConfig = () => {
+    return {
+      "NJ": {
+        fill: "#CC0000",
+      },
+      "NY": {
+        fill: "#CC0000"
+      },
+      "PA": {
+        fill: "#CC0000"
+      },
+      "FL": {
+        fill: "#CC0000"
+      }
+    };
+  };
+
   return (
     <div className='w-full bg-slate-50'>
       <div className='max-w-screen-xl w-full h-full flex flex-col mx-auto'>
-        test
+        {/* About Dell Painting section */}
+        <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-16'>
+          <div className='flex flex-row sm:flex-col justify-center md:justify-start items-center md:items-start pb-4'>
+            <div className='main-title-top uppercase'>
+              About
+            </div>
+            <div className='main-title-bottom uppercase'>
+              Dell Painting
+            </div>
+          </div>
+          <div className='col-span-1 md:col-span-2 lg:col-span-3 flex flex-col md:pl-8'>
+            <p>
+              Dell Painting is a <span className='font-bold'>Family Owned Business</span> providing Painting and Wallcovering services. 
+              Founded by Marcial Delgado who has <span className='font-bold'>25+ years of experience</span> in the industry the New York 
+              Metropolitan Area has trusted him and his team of professionals with their projects and 
+              ensuring <span className='font-bold'>all expectations are met</span>. The painting application process requires a unique 
+              level of skills and experience that we have perfected over the years. We recognize 
+              what’s important to Owners, Architects, General Contractors, Construction Managers and 
+              Property Managers when it comes to painting and our other services and we are completely 
+              confident that <span className='font-bold'>you can count on Dell Painting</span> to address your specific needs while 
+              delivering first-class results.          
+            </p>
+
+            <p className='pt-4 md:pt-8'>Get in touch with us today to recevie the best consultation and estimate from our experienced staff! </p>
+          </div>
+        </div>
+
+        {/* Where we operate section */}
+        <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-16'>
+
+          <div className='col-span-1 md:col-span-2 lg:col-span-3 flex flex-col justify-center items-center'>
+            <div className='max-w-[700px]'>
+              <UsaMap customize={statesCustomConfig()} width="100%" />
+            </div>
+          </div>
+
+          <div className='flex flex-row sm:flex-col justify-center md:justify-start items-center md:items-start pb-4 md:pl-8 order-first md:order-last'>
+            <div className='main-title-top uppercase'>
+              Where
+            </div>
+            <div className='main-title-bottom uppercase'>
+              We Operate
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect} from 'react'
-import bgVideo from '../assets/compressed.mp4'
+import bgVideo from '../assets/dellpainting-bg-vid.mp4'
 
 const Home = () => {
 
@@ -14,8 +14,8 @@ const Home = () => {
       {/* Container */}
       <div className='w-full justify-center h-full'>
         <video src={bgVideo} autoPlay loop muted playsInline/>
-        <div className='absolute w-full h-full top-0 flex flex-col justify-center p-4 md:p-8 items-center md:items-start'>
-          <div className='backdrop-blur-lg p-8'>
+        <div className='absolute w-full h-full top-0 flex flex-col justify-center '>
+          <div className='max-w-screen-xl w-full mx-auto backdrop-blur-lg p-8'>
             <p className='text-2xl md:text-6xl font-bold text-white drop-shadow-xl'>PAINTERS YOU CAN TRUST</p>
             <p className='md:text-3xl my-4 text-white drop-shadow-xl'>Over 25 years of experience at your service </p>
             <div className=''>
