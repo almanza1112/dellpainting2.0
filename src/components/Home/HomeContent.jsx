f01370364bdfc9938ad9b03c55e6920a0761b3b3
almanza1112 <almanza1112@gmail.com>
2022-07-30 00:16:52 -0400

    HomeContent finalized

--- src/components/Home/HomeContent.jsx
+++ src/components/Home/HomeContent.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import UsaMap from 'react-usa-map'
+import {FaAward, FaThumbsUp} from 'react-icons/fa'
+import {AiFillSafetyCertificate} from 'react-icons/ai'
+import {MdOutlineFamilyRestroom} from 'react-icons/md'
 
 const HomeContent = () => {
 
@@ -28,7 +31,8 @@ const HomeContent = () => {
     <div className='w-full bg-slate-50'>
       <div className='max-w-screen-xl w-full h-full flex flex-col mx-auto'>
         {/* About Dell Painting section */}
-        <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-16'>
+        <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-32'>
+          {/* title section */}
           <div className='flex flex-row sm:flex-col justify-center md:justify-start items-center md:items-start pb-4'>
             <div className='main-title-top uppercase'>
               About
@@ -37,6 +41,7 @@ const HomeContent = () => {
               Dell Painting
             </div>
           </div>
+          {/* description section */}
           <div className='col-span-1 md:col-span-2 lg:col-span-3 flex flex-col md:pl-8'>
             <p>
               Dell Painting is a <span className='font-bold'>Family Owned Business</span> providing Painting and Wallcovering services. 
@@ -54,21 +59,74 @@ const HomeContent = () => {
           </div>
         </div>
 
-        {/* Where we operate section */}
+        {/* Where We Operate section */}
         <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-16'>
-
+          {/* title section */}
           <div className='col-span-1 md:col-span-2 lg:col-span-3 flex flex-col justify-center items-center'>
             <div className='max-w-[700px]'>
-              <UsaMap customize={statesCustomConfig()} width="100%" />
+              <UsaMap customize={statesCustomConfig()} width="100%" title="Map of Operation for Dell Painting" />
             </div>
           </div>
-
+          {/* description section */}
           <div className='flex flex-row sm:flex-col justify-center md:justify-start items-center md:items-start pb-4 md:pl-8 order-first md:order-last'>
             <div className='main-title-top uppercase'>
               Where
             </div>
             <div className='main-title-bottom uppercase'>
               We Operate
+            </div>
+          </div>
+        </div>
+
+        {/* What Separates Us section */}
+        <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-32'>
+          {/* title section */}
+          <div className='flex flex-row sm:flex-col justify-center md:justify-start items-center md:items-start pb-4'>
+            <div className='main-title-top uppercase'>
+              What
+            </div>
+            <div className='main-title-bottom uppercase'>
+              Sets Us Apart
+            </div>
+          </div>
+          {/* description section */}
+          <div className='col-span-1 md:col-span-2 lg:col-span-3 grid md:grid-cols-2 md:pl-8 gap-8 md:gap-16'>
+            <div className='p-4 flex flex-col justify-center items-center'>
+              <FaAward size={40} color='#DC2626'/>
+              <p className='text-xl pt-4 sm:pt-10 pb-2 uppercase'>Quality</p>
+              <ul className='marker:text-red-600 list-outside list-disc ml-6 leading-8'>
+                <li>100% satisfaction guaranteed</li>
+                <li>We only use the best products</li>
+                <li>Professional tools for optimal results</li>
+              </ul>
+            </div>
+            <div className='p-4 flex flex-col justify-center items-center'>
+              <FaThumbsUp size={40} color='#DC2626'/>
+              <p className='text-xl pt-4 sm:pt-10 pb-2 uppercase'>Customer Satisfaction</p>
+              <ul className='marker:text-red-600 list-outside list-disc ml-6 leading-8'>
+                <li>Always meet project deadlines</li>
+                <li>Consistent high standard results</li>
+                <li>Regulary exceed job requirements</li>
+              </ul>
+            </div>
+           
+            <div className='p-4 flex flex-col justify-center items-center'>
+              <MdOutlineFamilyRestroom size={40} color='#DC2626' />
+              <p className='text-xl pt-4 sm:pt-10 pb-2 uppercase'>Family Owned</p>
+              <ul className='marker:text-red-600 list-outside list-disc ml-6 leading-8'>
+                <li>We treat you like family</li>
+                <li>Hardworking and experienced</li>
+                <li>First class results</li>
+              </ul>
+            </div>
+            <div className='p-4 flex flex-col justify-center items-center'>
+              <AiFillSafetyCertificate size={40} color='#DC2626' />
+              <p className='text-xl pt-4 sm:pt-10 pb-2 uppercase'>Safety</p>
+              <ul className='marker:text-red-600 list-outside list-disc ml-6 leading-8'>
+                <li>We are OSHA certified</li>
+                <li>Advocates Toolbox Talks</li>
+                <li>Personal Protective Equipment</li>
+              </ul>
             </div>
           </div>
         </div>
