8a5358467f440705a78bce4c3cfd07f673f5120b
almanza1112 <almanza1112@gmail.com>
2022-08-02 16:43:57 -0400

    Spackle section updated

--- src/components/Services/Spackle.jsx
+++ src/components/Services/Spackle.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { MdOutlineArrowBackIosNew } from 'react-icons/md'
+import SpackleImg from '../../assets/spackle.png'
 
 const Spackle = () => {
+
+  const myRef = useRef()
+  const executeScroll = () => myRef.current.scrollIntoView();
+  // Makes sure page starts on top of section when rendered
+  useEffect(() => {
+    executeScroll()
+  }, [])
+  
   return (
-    <div>Spackle</div>
+    <div ref={myRef} className='sss-style'>
+      <div className='sss-container'>
+        <Link to={'/services'} className='sss-back'>
+          <MdOutlineArrowBackIosNew size={20} /><span className='sss-back-text'>Other Services</span>
+        </Link>
+        <div className='sss-content'>
+          <div className='sss-text'>
+            <p className='sss-title'>Spackle</p>
+            <p>Repairing smaller damage to drywall or plaster. Filling small dings and dents, like nail holes in walls. Repair 
+              or fill a hole to produce a smooth surface.
+            </p>
+          </div>
+          <div className='sss-image'>
+            <img src={SpackleImg} />
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
