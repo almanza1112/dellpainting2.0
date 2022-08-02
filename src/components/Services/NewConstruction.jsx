a91644468c3820edb658d54bd5e67003856478ff
almanza1112 <almanza1112@gmail.com>
2022-08-02 14:23:08 -0400

    NewConstruction updated

--- src/components/Services/NewConstruction.jsx
+++ src/components/Services/NewConstruction.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { MdOutlineArrowBackIosNew } from 'react-icons/md'
+import NewConstructionImg from '../../assets/new_construction.jpg'
 
 const NewConstruction = () => {
+
+  const myRef = useRef()
+  const executeScroll = () => myRef.current.scrollIntoView();
+  // Makes sure page starts on top of section when rendered
+  useEffect(() => {
+    executeScroll()
+  }, [])
+
   return (
-    <div>NewConstruction</div>
-  )
+    <div ref={myRef} className='sss-style'>
+      <div className='sss-container'>
+        <Link to={'/services'} className='sss-back'>
+          <MdOutlineArrowBackIosNew size={20} /><span className='sss-back-text'>Other Services</span>
+        </Link>
+        <div className='sss-content'>
+          <div className='sss-text'>
+            <p className='sss-title'>New Construction</p>
+            <p>Interior Painting - Walls, Ceilings, Baseboard, Crown-Molding, Chair-rails, Doors and Frames, 
+              Shoe- molding, Wainscoting, Window Trims.
+            </p>
+          </div>
+          <img src={NewConstructionImg} />
+        </div>
+      </div>
+
+    </div>  
+    )
 }
 
 export default NewConstruction
