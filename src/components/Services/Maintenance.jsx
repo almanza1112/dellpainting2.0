6bd35ef947013ebb7e5242a5a574ee75728f69a1
almanza1112 <almanza1112@gmail.com>
2022-08-02 14:18:51 -0400

    services subsections styles created + maintenance

--- src/components/Services/Maintenance.jsx
+++ src/components/Services/Maintenance.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,{useEffect, useRef} from 'react'
+import {Link} from 'react-router-dom'
+import { MdOutlineArrowBackIosNew } from 'react-icons/md'
+import MaintenanceImg from '../../assets/maintenance.png'
 
 const Maintenance = () => {
+
+  const myRef = useRef()
+  const executeScroll = () => myRef.current.scrollIntoView();
+  // Makes sure page starts on top when rendered
+  useEffect(() => {
+    executeScroll()
+    //window.scrollTo(0, 0)
+  }, [])
+
   return (
-    <div>Maintenance</div>
+    <div ref={myRef} className='sss-style'>
+      <div className='sss-container'>
+        <Link to={'/services'} className='sss-back'>
+          <MdOutlineArrowBackIosNew size={20} /><span className='sss-back-text'>Other Services</span>
+        </Link>
+        <div className='sss-content'>
+          <div className='sss-text'>
+            <p className='sss-title'>Maintenance</p>
+            <p>Restoring original color on the walls. Updating Colors and Wallcovering for a 
+              more updated and unique look. Repairing any damages prior to restoring 
+              surfaces.
+            </p>
+          </div>
+          <img src={MaintenanceImg}/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
