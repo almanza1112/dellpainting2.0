5c230d149ea70ef85e82ed1450818aa16786289b
almanza1112 <almanza1112@gmail.com>
2022-08-04 18:34:11 -0400

    updated text and img

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import ServicesImg from '../assets/Services/services_main.jpg'
+import CareersImg from '../assets/Careers/careers_main.jpg'
 
 const Careers = () => {
 
@@ -10,7 +10,7 @@ const Careers = () => {
   
   return (
     <div className='w-full h-screen'>
-      <img src={ServicesImg} className='main-bg-img' />
+      <img src={CareersImg} className='main-bg-img' />
       <div className='main-content'>
         <div className='main-title-section'>
           <p className='main-title-top'>OUR</p>
@@ -19,7 +19,12 @@ const Careers = () => {
 
         <div className='main-desc-section'>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem id aspernatur, quibusdam accusantium repellendus officiis. Tempora ipsa itaque, dignissimos molestias at doloribus quas, id et, cumque deserunt odio! Consectetur.
+            Dell Painting is an equal opportunity employer and everyone is welcome. Every position 
+            is open for entry level to senior level, and there is always space to grow. Our working 
+            environment is a friendly and a strong team player company where we all work together 
+            to achieve Dell Painting's goals. Anyone interested in any of our available  positions
+            are welcome to fill out the form below or submit your resume to 
+            <span className='font-bold'> info@dellpainting.com</span>.
           </p>
         </div>
       </div>
