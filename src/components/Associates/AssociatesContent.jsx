5bd296c7f830d49cc7d7c42a5ee28aa82d9c4fc6
almanza1112 <almanza1112@gmail.com>
2022-08-05 01:45:14 -0400

    Associates logos added

--- src/components/Associates/AssociatesContent.jsx
+++ src/components/Associates/AssociatesContent.jsx
@@ -1,8 +1,218 @@
 import React from 'react'
+// Suppliers Imgs
+import BenjaminMooreImg from '../../assets/Associates/Suppliers/BenjaminMoorePaintsLogo.jpg'
+import EykonImg from '../../assets/Associates/Suppliers/eykon.png'
+import GracoImg from '../../assets/Associates/Suppliers/graco_logo.svg'
+import IdeaPaintImg from '../../assets/Associates/Suppliers/ideapaint.png'
+import KorosealImg from '../../assets/Associates/Suppliers/koroseal.png'
+import MatthewsPaintImg from '../../assets/Associates/Suppliers/matthews-paint.png'
+import MdcImg from '../../assets/Associates/Suppliers/BenjaminMoorePaintsLogo.jpg'
+//import MomentumtextilesImg from '../../assets/Associates/momentumtextiles.gif'
+import PPGPaintsImg from '../../assets/Associates/Suppliers/ppgpaints.png'
+import ScuffMasterImg from '../../assets/Associates/Suppliers/scuffmaster.png'
+import SherwinWilliamsImg from '../../assets/Associates/Suppliers/sherwinwilliams.png'
+import SunBeltRentalsImg from '../../assets/Associates/Suppliers/sunbeltrentals.svg'
+import TkPaintingsImg from '../../assets/Associates/Suppliers/tkpaintings.jpeg'
+import TristateEquipmentImg from '../../assets/Associates/Suppliers/tristateequipment.png'
+import UnitedRentalImg from '../../assets/Associates/Suppliers/unitedrental.png'
+import WolfGordonImg from '../../assets/Associates/Suppliers/wolfgordon.png'
+
+// Clients Imgs
+import AMCImg from '../../assets/Associates/Clients/amc.png'
+import ApplebeesImg from '../../assets/Associates/Clients/applebees.png'
+import ArborTerraceImg from '../../assets/Associates/Clients/arbor_terrace.png'
+import AtlanticHealthImg from '../../assets/Associates/Clients/atlantic_health_system.png'
+import AtriaSeniorLivingImg from '../../assets/Associates/Clients/atria-senior-living.png'
+import AutozoneImg from '../../assets/Associates/Clients/autozone.png'
+import BJsImg from '../../assets/Associates/Clients/bjs.png'
+import BNYMelonImg from '../../assets/Associates/Clients/bny_melon.png'
+import BrandyWineLivingImg from '../../assets/Associates/Clients/brandywineliving.png'
+import BrightViewImg from '../../assets/Associates/Clients/brightview-logo.png'
+import ChickfilaImg from '../../assets/Associates/Clients/chickfila.png'
+import ChowKingImg from '../../assets/Associates/Clients/chowking.png'
+import CMXImg from '../../assets/Associates/Clients/cmx-logo-1.png'
+import CostcoImg from '../../assets/Associates/Clients/costco.png'
+import CrunchFitnessImg from '../../assets/Associates/Clients/crunchfitness.png'
+import CubeSmartImg from '../../assets/Associates/Clients/cubesmart.png'
+import CVSImg from '../../assets/Associates/Clients/cvs-pharmacy.png'
+import DaveAndBustersImg from '../../assets/Associates/Clients/daveandbusters.png'
+import DevryUniversityImg from '../../assets/Associates/Clients/devryuniversity.png'
+import EnglewoodHealthImg from '../../assets/Associates/Clients/englewoodhealth.png'
+import EricksonLivingImg from '../../assets/Associates/Clients/erickson-living.png'
+import EtradeImg from '../../assets/Associates/Clients/etrade.png'
+import ExtraSpaceStorageImg from '../../assets/Associates/Clients/extraspacestorage.png'
+import FiddlersImg from '../../assets/Associates/Clients/fiddlers.jpeg'
+import FidelityImg from '../../assets/Associates/Clients/fidelity_investments.png'
+import FiveBelowImg from '../../assets/Associates/Clients/fivebelow.png'
+//import GivaudanImg from '../../assets/Associates/Clients/givaudan.svg'
+import HalalGuysImg from '../../assets/Associates/Clients/halalguys.png'
+import HiltonImg from '../../assets/Associates/Clients/hilton.png'
+import HotelImg from '../../assets/Associates/Clients/hotel.png'
+import HouseOfImg from '../../assets/Associates/Clients/houseof.png'
+import InsmedImg from '../../assets/Associates/Clients/insmed.png'
+import JaredImg from '../../assets/Associates/Clients/jared.png'
+import KayImg from '../../assets/Associates/Clients/kay.png'
+import KearnyBankImg from '../../assets/Associates/Clients/kearnybank.png'
+import KohlsImg from '../../assets/Associates/Clients/kohls.png'
+import L3Img from '../../assets/Associates/Clients/l3.png'
+import LanvinImg from '../../assets/Associates/Clients/lanvin.png'
+import LifestanceImg from '../../assets/Associates/Clients/lifestance.png'
+import MasonicVillagesImg from '../../assets/Associates/Clients/masonic-villages.png'
+import MaxMaraImg from '../../assets/Associates/Clients/maxmara.png'
+import MeadowLakesImg from '../../assets/Associates/Clients/meadow_lakes.png'
+import MercedesBenzImg from '../../assets/Associates/Clients/mercedesbenz.png'
+import MoesImg from '../../assets/Associates/Clients/moes.png'
+import MoltonBrownImg from '../../assets/Associates/Clients/moltonbrown.png'
+import MonroeVillageImg from '../../assets/Associates/Clients/monroevillage.png'
+import NovelPayImg from '../../assets/Associates/Clients/novelpay.png'
+import NuVasiveImg from '../../assets/Associates/Clients/nuVasive.jpeg'
+import OakStreetHealthImg from '../../assets/Associates/Clients/oakstreethealth.png'
+import OldNavyImg from '../../assets/Associates/Clients/oldnavy.png'
+import OshKoshImg from '../../assets/Associates/Clients/oshkosh_carters.png'
+import PeletonImg from '../../assets/Associates/Clients/peloton.png'
+import PetcoImg from '../../assets/Associates/Clients/petco.png'
+import PetsmartImg from '../../assets/Associates/Clients/petsmart.png'
+import PretImg from '../../assets/Associates/Clients/pret.png'
+import PTCImg from '../../assets/Associates/Clients/ptctherapeutics.png'
+import SalvatoreImg from '../../assets/Associates/Clients/salvatoreferragamo.png'
+import ShinolaImg from '../../assets/Associates/Clients/Shinola.png'
+import SignifyImg from '../../assets/Associates/Clients/signify.png'
+import SolvayImg from '../../assets/Associates/Clients/solvay.png'
+import StaplesImg from '../../assets/Associates/Clients/staples.png'
+import SubaruImg from '../../assets/Associates/Clients/subaru.png'
+import SunriseSeniorLivingImg from '../../assets/Associates/Clients/sunrise-senior-living.png'
+import TaihoImg from '../../assets/Associates/Clients/taiho.png'
+import TexasRoadhouseImg from '../../assets/Associates/Clients/texas_roadhouse.png'
+import TheHabitBurgerGrillImg from '../../assets/Associates/Clients/the_habit_burger_grill.png'
+import TripleFiveImg from '../../assets/Associates/Clients/triplefive.png'
+import VermellaImg from '../../assets/Associates/Clients/vermella.png'
+import WalmartImg from '../../assets/Associates/Clients/walmart.png'
+
 
 const AssociatesContent = () => {
   return (
-    <div>AssociatesContent</div>
+    <div className='w-full bg-slate-50'>
+        <div className='max-w-screen-xl w-full h-full flex flex-col mx-auto'>
+              {/* Suppliers section */}
+              <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-16'>
+                  {/* Logo section */}
+                  <div className='col-span-1 md:col-span-2 lg:col-span-3 flex flex-col justify-center items-center'>
+                    <div className='grid grid-cols-5 gap-16 p-8 justify-center items-center'>
+                          <img src={BenjaminMooreImg} />
+                          <img src={EykonImg} />
+                          <img src={GracoImg} />
+                          <img src={IdeaPaintImg} />
+                          <img src={KorosealImg} />
+                          <img src={MatthewsPaintImg} />
+                          <img src={MdcImg} />
+                          <img src={PPGPaintsImg} />
+                          <img src={SherwinWilliamsImg} />
+                          <img src={ScuffMasterImg} />
+                          <img src={SunBeltRentalsImg} />
+                          <img src={TkPaintingsImg} />
+                          <img src={TristateEquipmentImg} />
+                          <img src={UnitedRentalImg} />
+                          <img src={WolfGordonImg} />
+                    </div>
+                  </div>
+                  {/* Title section */}
+                  <div className='flex flex-row sm:flex-col justify-center md:justify-start items-center md:items-start pb-4 md:pl-8 order-first md:order-last'>
+                      <div className='main-title-top uppercase'>
+                          Our
+                      </div>
+                      <div className='main-title-bottom uppercase'>
+                          Suppliers
+                      </div>
+                  </div>
+              </div>
+
+              {/* Clients section */}
+              <div className='grid md:grid-cols-3 lg:grid-cols-4 p-4 md:px-8 md:divide-x py-4 md:py-16'>
+                  {/* Title section */}
+                  <div className='flex flex-row sm:flex-col justify-center md:justify-start items-center md:items-start pb-4 md:pl-8 order-first'>
+                      <div className='main-title-top uppercase'>
+                          Our
+                      </div>
+                      <div className='main-title-bottom uppercase'>
+                          Clients
+                      </div>
+                  </div>
+                  {/* Logo section */}
+                  <div className='col-span-1 md:col-span-2 lg:col-span-3 flex flex-col justify-center items-center'>
+                      <div className='grid grid-cols-5 gap-16 p-8 justify-center items-center'>
+                          <img src={AMCImg} />
+                          <img src={ApplebeesImg} />
+                          <img src={ArborTerraceImg} />
+                          <img src={AtlanticHealthImg} />
+                          <img src={AtriaSeniorLivingImg} />
+                          <img src={AutozoneImg} />
+                          <img src={BJsImg} />
+                          <img src={BNYMelonImg} />
+                          <img src={BrandyWineLivingImg} />
+                          <img src={BrightViewImg} />
+                          <img src={ChickfilaImg} />
+                          <img src={ChowKingImg} />
+                          <img src={CMXImg} />
+                          <img src={CostcoImg} />
+                          <img src={CrunchFitnessImg} />
+                          <img src={CubeSmartImg} />
+                          <img src={CVSImg} />
+                          <img src={DaveAndBustersImg} />
+                          <img src={DevryUniversityImg} />
+                          <img src={EnglewoodHealthImg} />
+                          <img src={EricksonLivingImg} />
+                          <img src={EtradeImg} />
+                          <img src={ExtraSpaceStorageImg} />
+                          <img src={FiddlersImg} />
+                          <img src={FidelityImg} />
+                          <img src={FiveBelowImg} />
+                          <img src={HalalGuysImg} />
+                          <img src={HiltonImg} />
+                          <img src={HotelImg} />
+                          <img src={HouseOfImg} />
+                          <img src={InsmedImg} />
+                          <img src={JaredImg} />
+                          <img src={KayImg} />
+                          <img src={KearnyBankImg} />
+                          <img src={KohlsImg} />
+                          <img src={L3Img} />
+                          <img src={LanvinImg} />
+                          <img src={LifestanceImg} />
+                          <img src={MasonicVillagesImg} />
+                          <img src={MaxMaraImg} />
+                          <img src={MeadowLakesImg} />
+                          <img src={MercedesBenzImg} />
+                          <img src={MoesImg} />
+                          <img src={MoltonBrownImg} />
+                          <img src={MonroeVillageImg} />
+                          <img src={NovelPayImg} />
+                          <img src={NuVasiveImg} />
+                          <img src={OakStreetHealthImg} />
+                          <img src={OldNavyImg} />
+                          <img src={OshKoshImg} />
+                          <img src={PeletonImg} />
+                          <img src={PetcoImg} />
+                          <img src={PetsmartImg} />
+                          <img src={PretImg} />
+                          <img src={PTCImg} />
+                          <img src={SalvatoreImg} />
+                          <img src={ShinolaImg} />
+                          <img src={SignifyImg} />
+                          <img src={SolvayImg} />
+                          <img src={StaplesImg} />
+                          <img src={SubaruImg} />
+                          <img src={SunriseSeniorLivingImg} />
+                          <img src={TaihoImg} />
+                          <img src={TexasRoadhouseImg} />
+                          <img src={TheHabitBurgerGrillImg} />
+                          <img src={TripleFiveImg} />
+                          <img src={VermellaImg} />
+                          <img src={WalmartImg} />
+                      </div>
+                  </div>
+              </div>
+        </div>
+    </div>
   )
 }
 
