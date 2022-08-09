e060c944e6741fb946e507e175ceb28d8f331e0d
almanza1112 <almanza1112@gmail.com>
2022-08-09 00:21:52 -0400

    routes logic added to portfolio

--- src/components/Portfolio/PortfolioContent.jsx
+++ src/components/Portfolio/PortfolioContent.jsx
@@ -1,52 +1,83 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import MaintenanceImg from '../../assets/Services/maintenance.png'
 
 const PortfolioContent = () => {
   return (
-    <div className='w-full flex flex-col p-4 md:px-8 md:py-16 bg-slate-50'>
+    <div className='w-full flex flex-col p-4 md:px-8 md:pb-16 bg-slate-50'>
         <div className='max-w-screen-xl w-full h-full mx-auto'>
               {/* Container */}
               <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 md:gap-16'>
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Apartments</span>
-                  </div>
+                  <Link to={'apartments'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Apartments</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Senior Livings</span>
-                  </div>
+                  <Link to={'seniorlivings'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Senior Livings</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Retail Stores</span>
-                  </div>
+                  <Link to={'retailstores'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Retail Stores</span>
+                      </div>
+                  </Link>
+                 
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Hotels</span>
-                  </div>
+                  <Link to={'hotels'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Hotels</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Entertainment</span>
-                  </div>
+                  <Link to={'entertainment'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Entertainment</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Restaurants</span>
-                  </div>
+                  <Link to={'restaurants'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Restaurants</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Banks</span>
-                  </div>
+                  <Link to={'banks'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Banks</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Offices</span>
-                  </div>
+                  <Link to={'offices'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Offices</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Car Dealerships</span>
-                  </div>
+                  <Link to={'cardealerships'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Car Dealerships</span>
+                      </div>
+                  </Link>
+                  
                   {/* Item */}
-                  <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
-                      <span className='card-title'>Self Storage</span>
-                  </div>
+                  <Link to={'selfstorage'}>
+                      <div style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${MaintenanceImg})` }} className='card portfolio-card-img'>
+                          <span className='card-title'>Self Storage</span>
+                      </div>
+                  </Link>
+                  
               </div>
         
         </div>
