b01f97ba4a36582de1226e0d71c5dd9990c5b87f
almanza1112 <almanza1112@gmail.com>
2022-08-15 01:29:27 -0400

    Home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,9 @@ const Home = () => {
       <div className='w-full justify-center h-full'>
         <video src={bgVideo} autoPlay loop muted playsInline/>
         <div className='absolute w-full h-full top-0 flex flex-col justify-center '>
-          <div className='max-w-screen-xl w-full mx-auto backdrop-blur-lg p-8'>
-            <p className='text-2xl md:text-6xl font-bold text-white drop-shadow-xl'>PAINTERS YOU CAN TRUST</p>
-            <p className='md:text-3xl my-4 text-white drop-shadow-xl'>Over 25 years of experience at your service </p>
+          <div className='max-w-screen-xl w-full mx-auto p-8'>
+            <p className='text-2xl md:text-6xl font-extrabold text-white drop-shadow-xl'>PAINTERS YOU CAN TRUST</p>
+            <p className='md:text-3xl my-4 text-white drop-shadow-xl font-normal'>Over 25 years of experience at your service </p>
             <div className=''>
               <button className='flat-btn-home'>GET IN TOUCH</button>
             </div>
